6d6aaf37c6e920b15e0a25c63471da3804de2833
cosmomojo <sonicismael2@gmail.com>
2023-07-07 10:40:40 +0100

    New favicon Update for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // git subtree push --prefix dist origin gh-pages
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/elaquil.co/',
   plugins: [react()],
 })
