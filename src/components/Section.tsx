2a6ef488e6013525fa5f40c38bbd4d78e92c3fbd
cosmomojo <sonicismael2@gmail.com>
2023-07-18 08:46:50 +0100

    Cleaned up a few components

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -16,11 +16,11 @@ const Section = ({id, title, children}: SectionProps) =>{
             <h1 className="secTitle secTitle3">{title}</h1>
             <h1 className="secTitle secTitle4">{title}</h1>
             <h1 className="secTitle secTitle5">{title}</h1>
-            <h1 className="secTitle secTitle1">{title}</h1>
-            <h1 className="secTitle secTitle2">{title}</h1>
-            <h1 className="secTitle secTitle3">{title}</h1>
-            <h1 className="secTitle secTitle4">{title}</h1>
-            <h1 className="secTitle secTitle5">{title}</h1>
+            <h1 className="secTitle secTitle6">{title}</h1>
+            <h1 className="secTitle secTitle7">{title}</h1>
+            <h1 className="secTitle secTitle8">{title}</h1>
+            <h1 className="secTitle secTitle9">{title}</h1>
+            <h1 className="secTitle secTitle10">{title}</h1>
         </div>
       </div>
       <br></br>
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -11,32 +11,5 @@ const ThemeContext = createContext<ThemeContextProps>({
 });
 
 export const ThemeContextProvider = ThemeContext.Provider;
-// const ThemeContextProvider = (children:any) => {
-
-//   const [theme, setTheme] = useState('light');
-
-//   useEffect(() => {
-//     const retrievedTheme = localStorage.getItem('theme');
-//     if (retrievedTheme === 'dark') {
-//       toggleTheme();
-//     }
-//   }, []);
-
-//   const toggleTheme = () => {
-//     if(theme === 'light') {
-//       document.documentElement.setAttribute('data-theme', 'dark');
-//       setTheme('dark');
-//     } else {
-//       document.documentElement.setAttribute('data-theme', 'light');
-//       setTheme('light');
-//     }
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
 
 export default ThemeContext;
