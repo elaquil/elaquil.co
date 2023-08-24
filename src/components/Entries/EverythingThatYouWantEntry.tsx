dac1fc56af75512a66cf54f285e183efb5cf89d9
cosmomojo <sonicismael2@gmail.com>
2023-08-24 12:10:24 +0100

    Finally a fix for the vimeo scaling on mobile, ported solution to JSX

--- src/components/Entries/EverythingThatYouWantEntry.tsx
+++ src/components/Entries/EverythingThatYouWantEntry.tsx
@@ -3,8 +3,14 @@ import Entry from "../Entry";
 const EverythingThatYouWantEntry = () => {
   return(
     <Entry title={"Everything (That You Want) (2022-) [After Effects, Mocha, Premiere, FFMPEG]"} subtitle={<h2><a href="https://open.spotify.com/artist/2KX08AwhdUadg1S4yurmce" target="blank">for @aloevittoria</a></h2>} >
-      <div className="IFrameWrapper">
-        <iframe src="https://player.vimeo.com/video/846363554?h=9e3508080b&autoplay=1&loop=1&muted=1" width="960" height="540" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+        <div style={{ padding: '56.25% 0 0 0', position: 'relative' }} className="IFrameWrapper">
+          <iframe
+            src="https://player.vimeo.com/video/846363554?h=9e3508080b&autoplay=1&loop=1&muted=1"
+            frameBorder={0}
+            allow="autoplay; fullscreen; picture-in-picture"
+            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+            title="200! Demo"
+          ></iframe>
       </div>
       <p>Music video for Aloe Vittoria's track "Everything (That You Want)", current WIP. Composited, rotoscoped, and post-processed in After Effects. Used FFMPEG to go in and break video data, leading to datamoshing and other digital garbage</p>
     </Entry>
