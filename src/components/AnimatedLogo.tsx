eb25c7ad19e0e9106c5a8327caa1ceb4c52aa3ad
cosmomojo <sonicismael2@gmail.com>
2023-09-26 11:02:18 +0100

    Fixed dark mode on animated logo

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
+import ThemeContext from "../context/ThemeContext";
 import './AnimatedLogo.css'
 
 const AnimatedLogo = () => {
+  const themeProviderItems = useContext(ThemeContext);
+  const { theme, toggleTheme } = themeProviderItems;
+
+
   return (
-    <div className='animatedLogoContainer'>
+    <div className={theme == 'dark' ? 'animatedLogoContainer dark' : 'animatedLogoContainer'}>
       <video autoPlay muted loop webkit-playsinline playsInline className='animatedLogo'>
         <source src="ElaquilCoLogoInOutWeb.mp4"></source>
       </video>
